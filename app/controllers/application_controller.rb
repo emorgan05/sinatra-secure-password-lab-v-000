27286f0be30b7bfc319f586e24404b161ed89b8f
Emily Morgan <emorgan05@gmail.com>
2017-11-06 21:45:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     binding.pry
     if params.include?(:username, :password)
-      
+
       user = User.new(username: params[:username], password: params[:password])
 
       if user.save
