2a6c492eefd55c84c69da3ea87cae0ddbced6435
Emily Morgan <emorgan05@gmail.com>
2017-11-06 21:44:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(username: params[:username], password: params[:password])
+    if params.include?(:username, :password)
+      user = User.new(username: params[:username], password: params[:password])
 
-    if user.save
-      redirect '/login'
+      if user.save
+        redirect '/login'
+      else
+        redirect '/failure'
+      end
     else
       redirect '/failure'
-    end
   end
 
   get '/account' do
